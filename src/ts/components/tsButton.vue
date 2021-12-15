25948ed135abffe173e4fdd7d465529b1a3d972d
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2021-12-15 15:39:14 +0900

    props test

--- src/ts/components/tsButton.vue
+++ src/ts/components/tsButton.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <Button class="button button-close" label="button"></Button>
+    <Button class="button button-close" :label="labelTxt"></Button>
   </div>
 </template>
  
 <script>
 import Button from 'primevue/button';
+import { ref } from 'vue'
 export default {
   name: 'tsButton',
   components: {
       Button
+  },
+  props: {
+    label: String
+  },
+  setup (props) {
+    const labelTxt = ref(props.label)
+    return {
+      labelTxt
+    }
   }
 }
 </script>
--- src/ui/guide/guideTsRadio.vue
+++ src/ui/guide/guideTsRadio.vue
@@ -2,6 +2,7 @@
     <div>
         <p>tsradio</p>
         <h1>counter : {{ counter }}</h1>
+        <ts-button :label="label"></ts-button>
     </div>
 </template>
 
@@ -11,8 +12,10 @@ import { ref } from 'vue'
 export default {
     setup() {
         const counter = ref(1)
+        const label = 'add'
         return {
-            counter
+            counter,
+            label
         }
     }
 }
