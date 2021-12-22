8ecd7cb942f09057d4c6bcc2f2697905fb977eee
lee kyung ryang <opp1350@naver.com>
2021-12-22 17:59:02 +0900

    svg 추가, app-menu 수정

--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,27 +1,38 @@
 <template>
-    <div>
-        <template v-for="item of menu" :key="item.name">
-            <div>
-                {{item.name}}
-            </div>
-            <ul>
-                <li v-for="child of item.children" :key="child.name">
-                    <router-link v-if="child.to" :to="child.to">
+    <ul class="app-manu">
+        <li class="first-depth" v-for="(item, perantIndex) of menu" :key="perantIndex">
+            <a href="javascript:void(0);" class="first-depth-item" :class="{'home' : item.name == 'Home'}">
+                <span>{{item.name}}</span>
+            </a>
+            <ul class="second-depth" v-if="item.children">
+                <li class="second-depth-item" v-for="(child, index) of item.children" :key="index">
+                    <router-link v-if="child.to" :to="child.to" @click="selectMenu(index, perantIndex)" :class="{'on' : selectedNum == index && selectedParentNum == perantIndex }">
                         {{child.name}}
                     </router-link>
                 </li>
             </ul>
-        </template>
-    </div>
+        </li>
+    </ul>
 </template>
 
 <script>
+import { ref } from 'vue';
 import menudata from '@/assets/menu/menu.json';
 export default {
     setup() {
         const menu = {...menudata.data}
+        const selectedNum = ref(0)
+        const selectedParentNum = ref(0)
+
+        const selectMenu = (idx, parentIdx) => {
+            selectedParentNum.value = parentIdx
+            selectedNum.value = idx
+        }
         return {
-            menu
+            menu,
+            selectedNum,
+            selectedParentNum,
+            selectMenu
         }
     },
 }
