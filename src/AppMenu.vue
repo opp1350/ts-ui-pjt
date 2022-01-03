9287ddb5100a770b45bea5a242f1298aa498d539
lee kyung ryang <opp1350@naver.com>
2022-01-03 16:07:22 +0900

    메뉴 수정

--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,15 +1,28 @@
 <template>
     <ul class="app-manu">
         <li class="first-depth" v-for="(item, perantIndex) of menu" :key="perantIndex">
-            <a class="first-depth-item" :class="{'home' : item.name == 'Home'}" @click="selectTab(perantIndex)" :aria-expanded="perantIndex == selectedParentNum" role="tab">
+            <a 
+                class="first-depth-item"
+                :class="{'home' : item.name == 'Home'}"
+                @click="selectTab(perantIndex)"
+                :aria-expanded="item.name !== 'Home' ? item.show : undefined"
+                :role="item.name !== 'Home' ? 'tab' : undefined"
+            >
                 <span>{{item.name}}</span>
             </a>
             <template v-if="item.children">
-                <transition name="fade">
-                    <ul class="second-depth" v-show="selectedParentNum == perantIndex">
-                        <li class="second-depth-item" v-for="(child, index) of item.children" :key="index">
+                <transition name="slide-up">
+                    <ul class="second-depth" v-show="item.show">
+                        <li
+                            class="second-depth-item"
+                            v-for="(child, index) of item.children" :key="index"
+                        >
                             <router-link
-                            v-if="child.to" :to="child.to" @click="selectMenu(index, perantIndex)" :class="{'on' : selectedNum == index && selectedParentNum == perantIndex }">
+                                v-if="child.to"
+                                :to="child.to"
+                                @click="selectMenu(index, perantIndex)"
+                                :class="{'on' : selectedNum == index && selectedParentNum == perantIndex }"
+                            >
                                 {{child.name}}
                             </router-link>
                         </li>
@@ -25,19 +38,19 @@ import { ref } from 'vue';
 import menudata from '@/assets/menu/menu.json';
 export default {
     setup() {
-        const menu = {...menudata.data}
+        const menuArr = ref([...menudata.data])
+        const menu = ref(menuArr.value.map(obj=> ({ ...obj, show: true })))
         const selectedNum = ref(0)
-        const selectedParentNum = ref(0)
+        const selectedParentNum = ref(1)
 
         const selectMenu = (idx, parentIdx) => {
             selectedParentNum.value = parentIdx
             selectedNum.value = idx
         }
         const selectTab = (parentIdx) => {
-            selectedParentNum.value == parentIdx ? selectedParentNum.value = -1 : selectedParentNum.value = parentIdx
-            
+            console.log(parentIdx, menu.value[parentIdx])
+            menu.value[parentIdx].show = !menu.value[parentIdx].show
         }
-        console.log(menu)
         return {
             menu,
             selectedNum,
@@ -48,19 +61,3 @@ export default {
     },
 }
 </script>
-<style scoped>
-/* fade 애니메이션 시작 (느리게 열기) */
-.fade-enter-active {
-    transition: all .8s ease-out;
-}
-/* fade 애니메이션 끝 (빠르게 닫기) */
-.fade-leave-active {
-    transition: all 0.45s cubic-bezier(0.190, 1.000, 0.220, 1.000);
-}
-.fade-enter-to, .fade-leave-from {
-    max-height: 1000px;
-}
-.fade-leave-to, .fade-enter-from {
-    max-height: 1px;
-}
-</style>
