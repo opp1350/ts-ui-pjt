fd86e782b93008cd16dfbf3c8ac1c2c87a4f732f
lee kyung ryang <opp1350@naver.com>
2022-01-03 12:56:43 +0900

    앱 메뉴 애니메이션 추가

--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,16 +1,21 @@
 <template>
     <ul class="app-manu">
         <li class="first-depth" v-for="(item, perantIndex) of menu" :key="perantIndex">
-            <a href="javascript:void(0);" class="first-depth-item" :class="{'home' : item.name == 'Home'}">
+            <a class="first-depth-item" :class="{'home' : item.name == 'Home'}" @click="selectTab(perantIndex)" :aria-expanded="perantIndex == selectedParentNum" role="tab">
                 <span>{{item.name}}</span>
             </a>
-            <ul class="second-depth" v-if="item.children">
-                <li class="second-depth-item" v-for="(child, index) of item.children" :key="index">
-                    <router-link v-if="child.to" :to="child.to" @click="selectMenu(index, perantIndex)" :class="{'on' : selectedNum == index && selectedParentNum == perantIndex }">
-                        {{child.name}}
-                    </router-link>
-                </li>
-            </ul>
+            <template v-if="item.children">
+                <transition name="fade">
+                    <ul class="second-depth" v-show="selectedParentNum == perantIndex">
+                        <li class="second-depth-item" v-for="(child, index) of item.children" :key="index">
+                            <router-link
+                            v-if="child.to" :to="child.to" @click="selectMenu(index, perantIndex)" :class="{'on' : selectedNum == index && selectedParentNum == perantIndex }">
+                                {{child.name}}
+                            </router-link>
+                        </li>
+                    </ul>
+                </transition>
+            </template>
         </li>
     </ul>
 </template>
@@ -28,12 +33,34 @@ export default {
             selectedParentNum.value = parentIdx
             selectedNum.value = idx
         }
+        const selectTab = (parentIdx) => {
+            selectedParentNum.value == parentIdx ? selectedParentNum.value = -1 : selectedParentNum.value = parentIdx
+            
+        }
+        console.log(menu)
         return {
             menu,
             selectedNum,
             selectedParentNum,
-            selectMenu
+            selectMenu,
+            selectTab
         }
     },
 }
 </script>
+<style scoped>
+/* fade 애니메이션 시작 (느리게 열기) */
+.fade-enter-active {
+    transition: all .8s ease-out;
+}
+/* fade 애니메이션 끝 (빠르게 닫기) */
+.fade-leave-active {
+    transition: all 0.45s cubic-bezier(0.190, 1.000, 0.220, 1.000);
+}
+.fade-enter-to, .fade-leave-from {
+    max-height: 1000px;
+}
+.fade-leave-to, .fade-enter-from {
+    max-height: 1px;
+}
+</style>
